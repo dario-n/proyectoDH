63f3971f5dc25ed059d7414789c2b63298a47f71
dario-n <dario.naldjian@gmail.com>
2017-09-27 14:40:16 -0300

    first commit

---
+++ includes/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Pagina</title>
+  </head>
+  <body>
---
+++ login.php
@@ -0,0 +1,4 @@
+<?php require_once("includes/head.php") ?>
+
+  </body>
+</html>
