59f0a9e2bed3e7c38ada7a47cc451bbad798e15b
pandrioli <pabloandrioli77@gmail.com>
2017-11-04 16:50:10 -0300

    paso a mySQL-final

--- functions/login_controller.php
+++ functions/login_controller.php
@@ -1,34 +1,31 @@
 <?php
-session_start();
 require_once('remember.php');
-$archivo = fopen('../json/usuarios.json', 'r');
+require_once('mysql_connection.php');
 
-if ($archivo) {
-  while (($linea = fgets($archivo)) !== false) {
-    $linea = json_decode($linea, true);
-    if ($linea['user'] == $_POST['user']) {
-      $_SESSION['error'] = '';
-      $_SESSION['user'] = $linea['user'];
-      break;
-    }
-    else {
-      $_SESSION['error'] = 'Los datos ingresados son incorrectos';
-      $_SESSION['user'] = $_POST['user'];
-      header('Location:../login.php');
-    }
-  }
-}
-fclose($archivo);
-
-if (password_verify($_POST['pwd'], $linea['pwd'])) {
-  $_SESSION['error'] = '';
-  $_SESSION['status'] = 1;
-  if ($_POST['recordar']) {
-    comprobarRemember();
-  }
-  header('Location:../index.php');
-}
-else {
+function error() {
   $_SESSION['error'] = 'Los datos ingresados son incorrectos';
+  $_SESSION['user'] = $_POST['user'];
   header('Location:../login.php');
 }
+
+$stmt = $db->prepare('SELECT * FROM users WHERE user = ?');
+$stmt->bindValue(1, $_POST['user'], PDO::PARAM_STR);
+$stmt->execute();
+$user = $stmt->fetch();
+
+
+if ($user) {
+  if (password_verify($_POST['pwd'], $user['pwd'])) {
+    $_SESSION['error'] = '';
+    $_SESSION['status'] = 1;
+    $_SESSION['user'] = $user['user'];
+    $_SESSION['nombre'] = $user['nombre'];
+    $_SESSION['apellido'] = $user['apellido'];
+    $_SESSION['mail'] = $user['mail'];
+    $_SESSION['img'] = $user['img'];
+    if ($_POST['recordar']) {
+      comprobarRemember();
+    }
+    header('Location:../index.php');
+  } else error();
+} else error();
--- perfil.php
+++ perfil.php
@@ -3,33 +3,16 @@
   if (!isset($_SESSION['status'])) {
     header("location: login.php"); exit;
   }
-  $archivo = fopen('./json/usuarios.json', 'r');
-
-  if ($archivo) {
-    while (($linea = fgets($archivo)) !== false) {
-      $linea = json_decode($linea, true);
-      if ($linea['user'] == $_SESSION['user']) {
-        $_SESSION['user'] = $linea['user'];
-        $_SESSION['nombre'] = $linea['nombre'];
-        $_SESSION['last_name'] = $linea['last_name'];
-        $_SESSION['mail'] = $linea['mail'];
-        $_SESSION['img'] = $linea['img'];
-        break;
-      }
-    }
-  }
-  fclose($archivo);
- ?>
- <?php $tabName = "Perfil" ?>
+ $tabName = "Perfil" ?>
  <?php require_once("includes/head.php") ?>
  <?php require_once("includes/navbar.php") ?>
  <section class="register-container">
    <br>
    <div class="usuario-perfil">
-     <img src="img/profiles/<?=$linea['img'];?>" align="left" width="150"alt="imagen-perfil">
-     <h1>Hola <?=$linea['nombre'];?> <?=$linea['last_name'];?></h1>
-     <h2>Alias "<?=$linea['user'];?>"</h2>
-     <h2>Tu mail: <?=$linea['mail'];?></h2>
+     <img src="img/profiles/<?=$_SESSION['img'];?>" align="left" width="150"alt="imagen-perfil">
+     <h1>Hola <?=$_SESSION['nombre'];?> <?=$_SESSION['apellido'];?></h1>
+     <h2>Alias "<?=$_SESSION['user'];?>"</h2>
+     <h2>Tu mail: <?=$_SESSION['mail'];?></h2>
    </div>
    <br>
    <a href="historial.php"><button type="button" name="historial">HISTORIAL DE DUELOS</button></a>
